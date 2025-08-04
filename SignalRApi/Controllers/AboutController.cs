278291c110767b77a63207410966dd108af9be08
Ozgur <Ozgur@DESKTOP-5RJ5ICK>
2025-08-04 18:40:50 +0300

    Ders 41'e Kadar. About Controller tamamlandı.

--- SignalRApi/Controllers/AboutController.cs
+++ SignalRApi/Controllers/AboutController.cs
@@ -35,7 +35,7 @@ namespace SignalRApi.Controllers
             _aboutService.TAdd(about);
             return Ok("Hakkımda kısmı başarılı şekilde eklendi");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteAbout(int id)
         {
             var value = _aboutService.TGetById(id);
@@ -55,7 +55,7 @@ namespace SignalRApi.Controllers
             _aboutService.TUpdate(about);
             return Ok("Hakkımda alanı güncellendi");
         }
-        [HttpGet("GetAbout")]
+        [HttpGet("{id}")]
         public IActionResult GetAbout(int id)
         {
             var value = _aboutService.TGetById(id);
